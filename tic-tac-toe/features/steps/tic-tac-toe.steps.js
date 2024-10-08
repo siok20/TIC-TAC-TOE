a11abd81a04bc6b6496f3857b0f40bb6f10face2
danicade24 <daniela.cadena.v@uni.pe>
2024-10-08 10:03:12 -0500

    Add scenario for draws

--- tic-tac-toe/features/steps/tic-tac-toe.steps.js
+++ tic-tac-toe/features/steps/tic-tac-toe.steps.js
@@ -33,7 +33,7 @@ Then('debería crearse una nueva partida con el tablero vacío', function () {
     expect(response).to.exist;
     expect(response.obj.board).to.equal('         '); // Nos debe devolver un tablero vacío
     expect(response.id).to.exist; // Verifica que se creo un id
-    });
+});
 });  
 
 // Paso para iniciar el juego entre dos jugadores
@@ -45,9 +45,9 @@ Given('un jugador llamado {string} comienza a jugar con {string}', function (jug
     socket.on('find', (data) => {
       response = data; // Guardar la respuesta para usarlo luego
       currentId = data.id;
-   });
+    });
 });
-  
+
 // Paso para realizar un movimiento
 
 When('{string} selecciona {string} en la posición {int}', function (jugador, marca, posicion) {
@@ -82,13 +82,26 @@ Then('debería ser el turno de {string}', async function (nombreSiguienteJugador
             const objToChange = data.objToChange;
             const isPlayer1Turn = objToChange.sum % 2 !== 0; // Si la suma es impar, es el turno de X
             const expectedPlayer = isPlayer1Turn ? objToChange.p1.name : objToChange.p2.name;
-
+            
             expect(expectedPlayer).to.equal(nombreSiguienteJugador);
             resolve(); // Resuelve la promesa para continuar con la prueba, esto se debe a que socket.on es asíncrono
         });
         resolve();
     });
 });
+
+// Paso para iniciar un juego entre dos jugadores
+Given('un jugador llamado {string} y un jugador llamado {string}', async function (jugador1, jugador2) {
+    socket = io.connect('http://localhost:4000');
+    // Emitir el evento para encontrar a ambos jugadores
+    socket.emit('find', { name: jugador1 });
+    socket.emit('find', { name: jugador2 });
+    
+    socket.on('find', (data) => {
+      response = data; // Guardar la respuesta para usarlo luego
+      currentId = data.id;
+   });
+})
 
 // Paso para verificar si hay un ganador
 Then('{string} debería ser el ganador', async function (nombreGanador) {
@@ -120,6 +133,27 @@ Then('el juego debería estar terminado', async function () {
             const game = playingArray.find(obj => obj.id === data.id);
             expect(game).to.exist; // Verifica que el juego exista
             expect(game.winner).to.equal(data.winner); // Verifica que el ganador coincida
+
+            resolve(); // Resuelve la promesa
+        });
+        resolve();
+    });
+});
+
+
+Then('el resultado debería ser empate', async function () {
+    return new Promise((resolve) => {
+        socket.on('gameOver', (data) => {
+            expect(data).to.be.an('object'); // Verifica que es un objeto
+
+            // Ahora verifica el ganador
+            expect(data.winner).to.exist;
+            expect(data.winner).to.be.oneOf([' - ', 'Ana', 'Mario']); // Verifica si el ganador es válido
+
+            // Verifica el estado del juego
+            const game = playingArray.find(obj => obj.id === data.id);
+            expect(game).to.exist; // Verifica que el juego exista
+            expect('-').to.equal(data.winner); // Verifica que el ganador coincida
 
             resolve(); // Resuelve la promesa
         });
--- tic-tac-toe/features/tic-tac-toe.feature
+++ tic-tac-toe/features/tic-tac-toe.feature
@@ -25,12 +25,36 @@ Característica: Juego Tic-Tac-Toe
         Cuando "Mario" selecciona "O" en la posición 2
         Entonces debería ser el turno de "Ana"
 
-    Escenario: Un jugador gana la partida
-        Dado un jugador llamado "Ana" comienza a jugar con "Mario"
-        Cuando "Ana" selecciona "X" en la posición 1
-        Y "Mario" selecciona "O" en la posición 2
-        Y "Ana" selecciona "X" en la posición 4
-        Y "Mario" selecciona "O" en la posición 5
-        Y "Ana" selecciona "X" en la posición 7
-        Entonces "Ana" debería ser el ganador
+    Esquema del escenario: Un jugador gana la partida
+        Dado un jugador llamado "<jugador1>" y un jugador llamado "<jugador2>"
+        Cuando "<jugador1>" selecciona "X" en la posición <posicion1>
+        Y "<jugador2>" selecciona "O" en la posición <posicion2>
+        Y "<jugador1>" selecciona "X" en la posición <posicion3>
+        Y "<jugador2>" selecciona "O" en la posición <posicion4>
+        Y "<jugador1>" selecciona "X" en la posición <posicion5>
+        Entonces "<jugador1>" debería ser el ganador
         Y el juego debería estar terminado
+
+        Ejemplos:
+        | jugador1 | jugador2 | posicion1 | posicion2 | posicion3 | posicion4 | posicion5 |
+        | Ana      | Mario    | 1         | 2         | 4         | 5         | 7         |
+        | Juan     | Pedro    | 1         | 3         | 5         | 2         | 6         |
+
+    Esquema del escenario: El juego termina en empate
+        Dado un jugador llamado "<jugador1>" y un jugador llamado "<jugador2>"
+        Cuando "<jugador1>" selecciona "X" en la posición <posicion1>
+        Y "<jugador2>" selecciona "O" en la posición <posicion2>
+        Y "<jugador1>" selecciona "X" en la posición <posicion3>
+        Y "<jugador2>" selecciona "O" en la posición <posicion4>
+        Y "<jugador1>" selecciona "X" en la posición <posicion5>
+        Y "<jugador2>" selecciona "O" en la posición <posicion6>
+        Y "<jugador1>" selecciona "X" en la posición <posicion7>
+        Y "<jugador2>" selecciona "O" en la posición <posicion8>
+        Y "<jugador1>" selecciona "X" en la posición <posicion9>
+        Entonces el juego debería estar terminado
+        Y el resultado debería ser empate
+
+        Ejemplos:
+            | jugador1 | jugador2 | posicion1 | posicion2 | posicion3 | posicion4 | posicion5 | posicion6 | posicion7 | posicion8 | posicion9 |
+            | Ana      | Mario    | 1         | 2         | 3         | 7         | 5         | 6         | 4         | 9         | 8         |
+            | Juan     | Pedro    | 1         | 2         | 4         | 3         | 5         | 7         | 8         | 6         | 9         |
